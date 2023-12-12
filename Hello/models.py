0a3ce8b51d4a2384df07465f06ba3e718f459d70
Bharatesh <skbharatesh1512@gmail.com>
2023-12-12 10:19:03 +0530

    Django project to host

---
+++ Hello/models.py
@@ -0,0 +1,11 @@
+from django.db import models
+
+# Create your models here.
+class students(models.Model):
+    student_ID=models.IntegerField()
+    student_name=models.CharField(max_length=50)
+    course= models.CharField(max_length=30)
+    jdate=models.DateField()
+    def __str__(self):
+        return self.student_name
+
---
+++ Hello/urls.py
@@ -0,0 +1,12 @@
+from django.contrib import admin
+from django.urls import path,re_path
+from Hello import views
+
+urlpatterns=[
+    path('admin/',admin.site.urls),
+re_path(r'^$', views.Hello, name='Hello'),
+re_path(r'^List/',views.list_view,name='list_view'),
+    ]
+
+
+
